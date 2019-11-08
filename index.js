de3fbf6f01c64602491777b2facb435e44a0dbe0
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-11-08 13:21:39 +0900

    first ver

--- index.js
+++ index.js
@@ -19,15 +19,34 @@ function fetchJSONFile(path, callback) {
 
 function recomend() {
     price = document.getElementById("price").value;
+    bag = [];
     try {
+        current_price = price;
         fetchJSONFile('https://gojiteji.github.io/kosen-festa2019_gacha/data.json', function (data) {
-            json=JSON.stringify(data[0]);
+            json = JSON.stringify(data);
             obj = JSON.parse(json);
-            console.log(obj[0]);
-            console.log(obj.剣道部);
-        });
 
-        document.getElementById("menu").innerHTML = data;
+            while (true) {
+                id = Math.floor(Math.random() * obj.length)
+                if (current_price >= obj[id].price) {
+                    current_price = current_price - obj[id].price;
+                    bag.push(id);
+                }
+
+                if (current_price < 50) {
+                    break;
+                }
+            }
+
+            text = "";
+            for (i = 0; i < bag.length; i++) {
+                text = text + obj[(bag[i])].group.toString() + " " + obj[(bag[i])].food.toString() + " " + obj[(bag[i])].price.toString() + "円<br>";
+            }
+
+            text = text + "総額：" + (price - current_price).toString() + "円";
+            document.getElementById("menu").innerHTML = text;
+
+        });
     }
     catch (e) {
         //alert("エラー! 入力をよく確認してくれ!");
