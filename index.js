3fb65bc884267c9a5035ac6acf532d8b4f8b381a
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-11-08 17:30:54 +0900

    2 ver

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function recomend() {
     price=price.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
     });
-
+    menu=""
     bag = [];
     if (!isNaN(price)) {
         try {
@@ -47,20 +47,31 @@ function recomend() {
 
 
 
-
-
-
                 text = "<h4>";
                 for (i = 0; i < bag.length; i++) {
+                    if(i<4){
+                    menu=menu+obj[(bag[i])].food.toString();
+                    }
+                    if(i<(bag.length-1)&&i<3){
+                        menu=menu+", "
+                    }
+                    
                     text = text + "<div class=\"ui card \" id= \"" + i + "\"><div class=\"content\"><div class=\"header\">"
                         + obj[(bag[i])].food.toString() + "<\/div><div class=\"meta\">" + obj[(bag[i])].group.toString() + "<\/div><div class=\"description\">"
                         + obj[(bag[i])].price.toString() + "円" + "</div></div></div>";
                 }
-
+                if(bag.length>3){
+                    menu=menu+"など"
+                }
+                tweet="location.href='https://twitter.com/share?url=https://gojiteji.github.io/kosen-festa2019_gacha/&text="+menu+"を食べるよ😋\\n"+"&hashtags=KOSENFESTA'"
+                document.getElementById("twb").onclick=new Function(tweet);
                 text = text + "</h4><h2>総額：" + (price - current_price).toString() + "円</h2>";
                 document.getElementById("menu").innerHTML = text;
 
             });
+
+           
+            
         }
         catch (e) {
             //alert("エラー! 入力をよく確認してくれ!");
@@ -68,7 +79,7 @@ function recomend() {
 
     }else{
         document.getElementById("price").value=""
-        document.getElementById("price").placeholder="50~9999を入力してね！"
+        document.getElementById("price").placeholder="数字だけを入力してね!"
     }
 }
 
