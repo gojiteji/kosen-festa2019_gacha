8fae795a985768a993745c88e55d45cacc1f8aaa
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-11-08 11:30:55 +0900

    json

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function recomend() {
     try {
         fetchJSONFile('https://gojiteji.github.io/kosen-festa2019_gacha/data.json', function (data) {
             json=JSON.stringify(data[0])
-            console.log(json[0][0]);
-            console.log(json[0]['剣道部']);
+            console.log(json)
+            console.log(json[0]);
+            console.log(json['剣道部']);
         });
 
         document.getElementById("menu").innerHTML = data;
